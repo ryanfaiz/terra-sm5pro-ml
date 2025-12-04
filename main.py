05244476c9921ab9ea99f87f971d8a3b798f885b
Ryan Faiz Sanie <hi@ryanfaiz.com>
2025-12-04 09:14:01 +0700

    fix: allowed origin

--- main.py
+++ main.py
@@ -448,7 +448,8 @@ app.add_middleware(
         "http://localhost:3000",           
         "http://localhost:8000",  
         "http://127.0.0.1:8000",  
-        "https://*.railway.app"        
+        "https://*.railway.app",
+        "https://*.cervosys.app",     
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
